ce7eac7509d440c9de4ea432ed0f89e0f13a70a5
shcherbana <shcherba.pn@ucu.edu.ua>
2023-11-29 20:40:51 +0200

    Adding Cashe Document and Timed Document

---
+++ src/main/java/ua/edu/ucu/apps/CasheDocument.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class CacheDocument implements Document {
+
+    private final String path;
+    private static final String SELECT_QUERY = "SELECT text FROM documents WHERE path = ?";
+    private static final String INSERT_QUERY = "INSERT INTO documents (path, text) VALUES (?, ?)";
+
+    @Override
+    public String parse() throws SQLException, IOException {
+        String text = getTextFromCache(path);
+
+        if (text == null) {
+            text = new SmartDocument(path).parse();
+            saveTextToCache(text);
+        }
+
+        return text;
+    }
+
+    private String getTextFromCache(String path) throws SQLException {
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:identifier.sqlite");
+             PreparedStatement selectStatement = conn.prepareStatement(SELECT_QUERY)) {
+            selectStatement.setString(1, path);
+
+            try (ResultSet resultSet = selectStatement.executeQuery()) {
+                return resultSet.next() ? resultSet.getString("text") : null;
+            }
+        }
+    }
+
+    private void saveTextToCache(String text) throws SQLException {
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:identifier.sqlite");
+             PreparedStatement insertStatement = conn.prepareStatement(INSERT_QUERY)) {
+            insertStatement.setString(1, path);
+            insertStatement.setString(2, text);
+            insertStatement.executeUpdate();
+        }
+    }
+}
---
+++ src/main/java/ua/edu/ucu/apps/TimedDocument.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class TimedDocument implements Document {
+    public String path;
+
+    @Override
+    public String parse() throws IOException, SQLException {
+        String smartTxt = measureTime(new SmartDocument(path));
+        String cachedTxt = measureTime(new CachedDocument(path));
+
+        return cachedTxt != null ? cachedTxt : smartTxt;
+    }
+
+    private String measureTime(Document doc) throws IOException, SQLException {
+        long startTime = System.currentTimeMillis();
+        String result = doc.parse();
+        long endTime = System.currentTimeMillis();
+
+        logExecutionTime(doc, endTime - startTime);
+
+        return result;
+    }
+
+    private void logExecutionTime(Document doc, long executionTime) {
+        String type = (doc instanceof SmartDocument) ? "SmartDocument" : "CachedDocument";
+        System.out.println("Time of " + type + ": " + executionTime / 1000.0);
+    }
+}
